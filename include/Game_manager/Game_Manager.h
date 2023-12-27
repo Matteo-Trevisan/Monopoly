14a994f62d53deedf9fab93bb459ec3cdda33a78
Matteo Trevisan <64227186+Matteo-Trevisan@users.noreply.github.com>
2023-12-27 17:06:47 +0100

    Generate Game_Manager class

---
+++ include/Game_manager/Game_Manager.h
@@ -0,0 +1,15 @@
+
+#ifndef GAME_MANAGER_H
+#define GAME_MANAGER_H
+
+
+class Game_Manager {
+public:
+
+private:
+
+
+};
+
+
+#endif //GAME_MANAGER_H
---
+++ src/Game_manager/Game_Manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matteo Trevisan on 27/12/23.
+//
+
+#include "Game_manager/Game_Manager.h"
