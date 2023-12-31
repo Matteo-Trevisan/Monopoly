8db0ffc87f6edbc216b0e5fe5f01121020403359
Elia <elia.tolfo@studenti.unipd.it>
2023-12-31 19:41:16 +0100

    Cambiato condizione current_player.has_enough_money(arrival_space.get_terrain_sale_price()

--- src/Game_manager/Game_Manager.cpp
+++ src/Game_manager/Game_Manager.cpp
@@ -180,7 +180,7 @@ void Game_Manager::run_game() {
 
 		// Se la casella non è di nessuno offre di comprarla
 		if (arrival_space.get_owner() == nullptr) {		// TODO ti chiede se vuoi aquistarla anche se non hai abbastanza soldi: da cambiare
-			if(arrival_space.get_terrain_sale_price() < current_player.get_balance()) {
+			if(current_player.has_enough_money(arrival_space.get_terrain_sale_price())) {
                 bool offer = current_player.offer(
                         "Giocatore " + current_player.get_name() + ", vuoi acquistare la proprietà " +
                         arrival_space.get_name() + " a " + std::to_string(arrival_space.get_terrain_sale_price()) +
